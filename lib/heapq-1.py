73ad71d21d620134cf65f1f11b26c8c2ed993f99
jkb <a85479094@gmail.com>
2022-12-13 21:55:03 +0900

    2022.12.13

--- lib/heapq-1.py
+++ lib/heapq-1.py
@@ -2,20 +2,20 @@ import heapq
 
 # 1. 힙 정렬(오름차순)
 # 원소를 힙에 넣었다가 빼는 것만으로도 오름차순 정렬이 완료된다.
-# def heapsort(iterable):
-#     h = []
-#     result = []
-#     # iterable의 모든 원소를 h에 삽입
-#     for value in iterable:
-#         heapq.heappush(h, value)
-#     # h(힙)에 삽입된 모든 원소를 차례대로 꺼내어 result에 넣기
-#     for _ in range(len(h)):
-#         result.append(heapq.heappop(h))
-#     return result
+def heapsort(iterable):
+    h = []
+    result = []
+    # iterable의 모든 원소를 h에 삽입
+    for value in iterable:
+        heapq.heappush(h, value)
+    # h(힙)에 삽입된 모든 원소를 차례대로 꺼내어 result에 넣기
+    for _ in range(len(h)):
+        result.append(heapq.heappop(h))
+    return result
 
-# a = [1, 3, 5, 7, 9, 2, 4, 6, 8, 0]
-# result = heapsort(a)
-# print(result)
+a = [1, 3, 5, 7, 9, 2, 4, 6, 8, 0]
+result = heapsort(a)
+print(result)
 
 
 
